cf2f86b5200f40f41c1e1c040c5d0e1a28580be2
pathus <panienkaczuprynienka@gmail.com>
2023-01-30 21:51:38 +0100

    test attribute

--- src/test/java/tests/FailedLoginTests.java
+++ src/test/java/tests/FailedLoginTests.java
@@ -15,7 +15,7 @@ public class FailedLoginTests extends TestBase {
   @Issue("DEFECT-1")
   @TmsLink("ID-1")
   @Severity(SeverityLevel.NORMAL)
-  @Test
+  @Test(enabled = true)
   @Description("The goal of this test is to log in using not proper username and password" +
           " and check if warning message Invalid username or password is displayed")
   public void asUserTryToLogInWithIncorrectLoginAndPassword() {
